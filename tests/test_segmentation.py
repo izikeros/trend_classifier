52e18f1b366258bc68633e0dc2176449e5560336
Krystian Safjan <ksafjan@gmail.com>
2022-09-08 17:25:59 +0200

    Fix tests

--- tests/test_segmentation.py
+++ tests/test_segmentation.py
@@ -1,35 +1,39 @@
+import pytest
+
 from trend_classifier.configuration import Config
 from trend_classifier.segmentation import Segmenter
 
 
 class TestCalculateSegments:
-    def setup_method(self):
-        self.seg = Segmenter(config=Config())
-
     def test_calculate_segments__lambda_shape(self):
-        self.seg.x = list(range(0, 200))
-        self.seg.y = list(range(0, 100)) + list(range(100, 0, -1))
+        x = list(range(0, 200))
+        y = list(range(0, 100)) + list(range(100, 0, -1))
+        self.seg = Segmenter(x=x, y=y)
+
         segments = self.seg.calculate_segments()
         # FIXME: KS: 2022-09-01: Expecting to have 2 segments,
         #  but currently getting 3 with default config
         assert len(segments) == 3
 
     def test_calculate_segments__v_shape(self):
-        self.seg.x = list(range(0, 200))
-        self.seg.y = list(range(100, 0, -1)) + list(range(0, 100))
+        x = list(range(0, 200))
+        y = list(range(100, 0, -1)) + list(range(0, 100))
+        self.seg = Segmenter(x=x, y=y)
         segments = self.seg.calculate_segments()
         assert len(segments) == 2
 
     def test_calculate_segments__line_up(self):
-        self.seg.x = list(range(0, 200))
-        self.seg.y = list(range(0, 200))
+        x = list(range(0, 200))
+        y = list(range(0, 200))
+        self.seg = Segmenter(x=x, y=y)
         segments = self.seg.calculate_segments()
         # FIXME: KS: 2022-09-02: Expecting to have 1 segment, but currently getting 3
         assert len(segments) == 3
 
     def test_calculate_segments__line_down(self):
-        self.seg.x = list(range(0, 200))
-        self.seg.y = list(range(200, 0, -1))
+        x = list(range(0, 200))
+        y = list(range(200, 0, -1))
+        self.seg = Segmenter(x=x, y=y)
         segments = self.seg.calculate_segments()
         assert len(segments) == 1
 
