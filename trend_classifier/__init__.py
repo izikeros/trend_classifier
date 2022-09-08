8301050c6e2aa93f7d22221434635cc6ca290e65
Krystian Safjan <ksafjan@gmail.com>
2022-09-08 14:13:50 +0200

    Add __all__ to package init

--- trend_classifier/__init__.py
+++ trend_classifier/__init__.py
@@ -1,1 +1,9 @@
 """Package for automated trend classification."""
+from trend_classifier.configuration import CONFIG_ABS
+from trend_classifier.configuration import CONFIG_REL
+from trend_classifier.configuration import Config
+from trend_classifier.segment import Segment
+from trend_classifier.segmentation import Segmenter
+from trend_classifier.types import FigSize
+
+__all__ = ["Segmenter", "Segment", "Config", "CONFIG_ABS", "CONFIG_REL", FigSize]
