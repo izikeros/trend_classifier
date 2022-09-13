1df1bcc26b64519f66c5cc75bb4524e48e0596c2
Krystian Safjan <ksafjan@gmail.com>
2022-09-13 10:38:50 +0200

    Change config used in tests, add new tests

--- tests/test_segmentation.py
+++ tests/test_segmentation.py
@@ -1,6 +1,7 @@
 from unittest.mock import patch
 
 import pytest
+from trend_classifier.configuration import CONFIG_REL_SLOPE_ONLY
 from trend_classifier.segmentation import Segmenter
 
 
@@ -8,27 +9,27 @@ class TestCalculateSegments:
     def test_calculate_segments__lambda_shape(self):
         x = list(range(0, 200))
         y = list(range(0, 100)) + list(range(100, 0, -1))  # noqa: FKA01
-        self.seg = Segmenter(x=x, y=y)
+        self.seg = Segmenter(x=x, y=y, config=CONFIG_REL_SLOPE_ONLY)
 
         segments = self.seg.calculate_segments()
-        # FIXME: KS: 2022-09-01: Expecting to have 2 segments,
-        #  but currently getting 3 with default config
-        assert len(segments) == 3
+        n_segments = len(segments)
+        assert n_segments == 2
 
     def test_calculate_segments__v_shape(self):
         x = list(range(0, 200))
         y = list(range(100, 0, -1)) + list(range(0, 100))  # noqa: FKA01
-        self.seg = Segmenter(x=x, y=y)
+        self.seg = Segmenter(x=x, y=y, config=CONFIG_REL_SLOPE_ONLY)
         segments = self.seg.calculate_segments()
-        assert len(segments) == 2
+        n_segments = len(segments)
+        assert n_segments == 2
 
     def test_calculate_segments__line_up(self):
         x = list(range(0, 200))
         y = list(range(0, 200))
-        self.seg = Segmenter(x=x, y=y)
+        self.seg = Segmenter(x=x, y=y, config=CONFIG_REL_SLOPE_ONLY)
         segments = self.seg.calculate_segments()
-        # FIXME: KS: 2022-09-02: Expecting to have 1 segment, but currently getting 3
-        assert len(segments) == 3
+        n_segments = len(segments)
+        assert n_segments == 1
 
     def test_calculate_segments__line_down(self):
         x = list(range(0, 200))  # noqa: FKA01
@@ -53,6 +54,16 @@ class TestCalculateSegments:
         area_outside_trend = self.seg.calc_area_outside_trend()
         assert area_outside_trend > 0
 
+    def test__set_offset(self):
+        self.seg = Segmenter(x=[], y=[])
+        offset = self.seg._set_offset(n=100, overlap_ratio=0.2)
+        assert offset == 20
+
+    def test__set_offset__adjust_to_one(self):
+        self.seg = Segmenter(x=[], y=[])
+        offset = self.seg._set_offset(n=10, overlap_ratio=0.001)
+        assert offset == 1
+
 
 class TestSegmenterPlotting:
     def setup_class(self):
@@ -72,6 +83,10 @@ class TestSegmenterPlotting:
     @patch("matplotlib.pyplot.show")
     def test_plot_detrended_signal(self, mock_show):
         self.seg.plot_detrended_signal()
+
+    @patch("matplotlib.pyplot.show")
+    def test_plot_segment_with_trendlines_no_context(self, mock_show):
+        self.seg.plot_segment_with_trendlines_no_context(0)
 
 
 @pytest.mark.skip(reason="Use for manual testing")
