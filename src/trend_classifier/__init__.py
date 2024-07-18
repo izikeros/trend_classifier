61213b4a59e1e1c276afe15e8d0d29314536c526
Krystian Safjan <ksafjan@gmail.com>
2024-07-18 15:30:00 +0200

    Bump version: 0.1.12 → 0.2.0

--- src/trend_classifier/__init__.py
+++ src/trend_classifier/__init__.py
@@ -5,4 +5,4 @@ from trend_classifier.segment import Segment
 from trend_classifier.segmentation import Segmenter
 
 __all__ = ["Segmenter", "Segment", "Config", "CONFIG_ABS", "CONFIG_REL"]
-__version__ = "0.1.12"
+__version__ = "0.2.0"
